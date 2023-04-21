9456ee885f00547a5828f9566316044129cf7d83
romeiaAlexander <romeialexander02@gmail.com>
2023-04-21 18:30:36 +0800

    first commit

---
+++ biodata/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+    <link href="../biodata/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
+    <h1>Biodata</h1>
+    <img src="../biodata/tpe muka.jpeg" width="120px" height="160px">
+        <table>
+            <tr>
+                <th>Name</th>
+                <td><?php echo "Romei Manahmpi"; ?></td>
+            </tr>
+            <tr>
+                <th>Birth Date & Place</th>
+                <td><?php echo "Manado, 25 Febuary 2002"; ?></td>
+            </tr>
+            <tr>
+                <th>Gender</th>
+                <td><?php echo "Male"; ?></td>
+            </tr>
+            <tr>
+                <th>Address</th>
+                <td><?php echo "Banjer, Tikala, Manado"; ?></td>
+            </tr>
+            <tr>
+                <th>Education</th>
+                <td>
+                    <?php echo "Elementary : SD Kristen Eben Haezar Manado"; ?>
+                    <br>
+                    <?php echo "Junior High School : SMP Kristen Eben Haezar Manado"; ?>
+                    <br>
+                    <?php echo "Senior High School : SMA Negeri 7 Manado"; ?>
+                    <br>
+                    <?php echo "College : Sam Ratulangi University"; ?>
+                </td>
+            </tr>
+            <tr>
+                <th>Hobby</th>
+                <td><?php echo "Playing games and watching anime"; ?></td>
+            </tr>
+        </table>
+
+</body>
+</html>
